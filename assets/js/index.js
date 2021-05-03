c2a8e87dbd637879ea60daff29ce617ac547573a
denis-dashkevich-idf <denis.dashkevich@moneyman.ru>
2021-05-03 21:08:45 +0300

    add grid, layout header

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+// header
+let header = document.querySelector(".header");
+
+document.addEventListener("scroll", () => {
+  if (document.documentElement.scrollTop > 1) {
+    header.classList.add("header_has-bg");
+  } else {
+    header.classList.remove("header_has-bg");
+  }
+});
