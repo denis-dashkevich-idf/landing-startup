fb18d066142b98e77153883aa7be4cf4c4e01257
denis-dashkevich-idf <denis.dashkevich@moneyman.ru>
2021-05-12 21:19:38 +0300

    add script concat

---
+++ assets/js/components/mobileMenu/index.js
@@ -0,0 +1,17 @@
+// mobile menu
+(function () {
+  const header = 'hed';
+  const burger = document.querySelector(".burger");
+  const menuContainerEl = document.querySelector(".header__menu-container");
+  
+  burger.addEventListener('click', function() {
+    this.classList.toggle('burger_closed');
+    menuContainerEl.classList.toggle('header__menu-container_visible');
+    header.classList.toggle('header_menu-opend');
+    document.body.classList.toggle('content-hidden');
+  
+  });
+
+  console.log(header);
+
+})();
